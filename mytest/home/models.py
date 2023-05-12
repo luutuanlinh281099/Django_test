673ef2c587934343f27f58ab8b63b5f374c8f4e6
luutuanlinh281099 <tuanlinh99lfc@gmail.com>
2023-05-12 18:41:16 +0700

    l

--- mytest/home/models.py
+++ mytest/home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Question (models.Model):
+    question_id = models.IntegerField()
+    question_text = models.CharField(max_length=200)
+    time = models.DateTimeField
+
+class Choice (models.Model):
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=300)
+    vote = models.IntegerField(default=0)
--- mytest/home/views.py
+++ mytest/home/views.py
@@ -3,5 +3,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    # return HttpResponse('Linh ngoan hien')
-    return render(request, "home.html")
+    my_name = 'Linh ngoan hien'
+    my_age = '24'
+    my_asset = {'Thuy', 'Cua'}
+    json = {'name' : my_name, 'age' : my_age, 'asset' : my_asset}
+    return render(request, "home/home.html", json)
